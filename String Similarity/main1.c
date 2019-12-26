17b6a1ea3779a449c608d96aa586c5c629c1c1f3
aakif-h <54697234+aakif-h@users.noreply.github.com>
2019-12-26 12:22:15 -0800

    Create main1.c
    This implementation of the problem has a worst case time complexity of O(n^2).

---
+++ String Similarity/main1.c
@@ -0,0 +1,86 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+long checkSimilarities(char* s, int start) {
+    int similarities = 0;
+    for (int i = start; i < strlen(s); i++) {
+        if (s[i - start] != s[i]) {
+            break;
+        }
+        similarities++;
+    }
+    return similarities;
+}
+
+// Complete the stringSimilarity function below.
+long stringSimilarity(char* s) {
+    long total = strlen(s);
+    for (int i = 1; i < strlen(s); i++) {
+        total += checkSimilarities(s, i);
+    }
+    return total;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char* s = readline();
+
+        long result = stringSimilarity(s);
+
+        fprintf(fptr, "%ld\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
