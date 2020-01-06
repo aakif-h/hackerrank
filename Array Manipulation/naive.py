94257e9c740237b255c9cd6f8747b50f6c65e207
aakif-h <54697234+aakif-h@users.noreply.github.com>
2020-01-05 23:18:03 -0800

    Create naive.py
    The naive solution to the problem. Operates with a time complexity of O(n^2).

---
+++ Array Manipulation/naive.py
@@ -0,0 +1,32 @@
+def arrayManipulation(n, queries):
+    arr = [0] * (n + 1)
+    for query in queries:
+        for i in range(query[0], query[1] + 1):
+            arr[i] += query[2]
+
+    maximum = -sys.maxsize - 1
+    for i in range(n):
+        if arr[i] > maximum:
+            maximum = arr[i]
+
+    return maximum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
